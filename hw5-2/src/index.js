ce5fec5c914619818ec53cae06ac8139c8000414
ShimYoungjun <jacob010605@gmail.com>
2024-11-23 23:09:20 +0900

    Update

--- hw5-2/src/index.js
+++ hw5-2/src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
